0ad1f68ce082901f7d01a8de9854770faffcb1b1
Johami <johamiangelinesilvamedina@gmail.com>
2023-11-30 21:34:30 -0500

    Se modifico html, css y js, para hacer que el menu de navegación fuera responsive y se agrego una imagen que hacia falta (icono de whatsapp)

---
+++ Js/script.js
@@ -0,0 +1,6 @@
+// Menu Responsive
+bars = document.querySelector(".bars");
+bars.onclick = function () {
+    navBar = document.querySelector(".menuNavegacion");
+    navBar.classList.toggle("active");
+}
